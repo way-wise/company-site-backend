414a0860f9e84083cb94266e47d0fef237319966
sakil-sagor <sakilsagor76543@gmail.com>
2025-10-20 21:55:30 +0600

    update database

--- src/shared/prismaClient.ts
+++ src/shared/prismaClient.ts
@@ -1,8 +1,5 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-}).$extends(withAccelerate());
+let prisma = new PrismaClient();
 
 export default prisma;
