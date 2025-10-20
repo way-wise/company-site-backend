4bf6d7c82d5b70e7f755320508994007f287d173
sakil-sagor <sakilsagor76543@gmail.com>
2025-10-20 20:17:48 +0600

    remove local

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,6 @@ app.use(
       "https://waywisetech.com",
       "https://company-site-frontend.vercel.app",
       "http://localhost:3000",
-      "http://127.0.0.1:3000",
       "http://192.168.1.37:3000",
     ],
     credentials: true,
