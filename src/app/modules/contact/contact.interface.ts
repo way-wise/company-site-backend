a45e14794ee94b56c5620e9be9c0c9c02e9a628c
A K M Sakil Sagor <sakilsagor76543@gmail.com>
2025-11-20 15:18:07 +0600

    ww

--- src/app/modules/contact/contact.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IContactFormData {
-  fullName: string;
-  email: string;
-  whatsappNumber: string;
-  serviceRequired: string;
-  projectBudget: string;
-  projectDescription: string;
-}
-
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import { AdminRoutes } from "../modules/admin/admin.routes";
 import { authRoutes } from "../modules/auth/auth.routes";
 import { ChatRoutes } from "../modules/chat/chat.routes";
-import { ContactRoutes } from "../modules/contact/contact.routes";
 import { leaveRoutes } from "../modules/leave/leave.routes";
 import { leaveBalanceRoutes } from "../modules/leaveBalance/leaveBalance.routes";
 import { MilestoneRoutes } from "../modules/milestone/milestone.routes";
@@ -73,10 +72,6 @@ const moduleRoutes = [
   {
     path: "/partner",
     route: PartnerRoutes,
-  },
-  {
-    path: "/contact",
-    route: ContactRoutes,
   },
 ];
 
