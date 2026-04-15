2611c0b736928a139323e4b6442fcf841cbd6e6a
rakib0101 <rakib.01jan@gmail.com>
2026-04-15 15:41:51 +0600

    feat: ckeditor added

--- src/app.ts
+++ src/app.ts
@@ -69,7 +69,8 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
 // Swagger Documentation
